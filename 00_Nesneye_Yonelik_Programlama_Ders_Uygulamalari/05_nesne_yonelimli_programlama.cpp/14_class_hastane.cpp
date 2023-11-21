70a2bc85e872dd9242290f5f3ae57b9b98676380
Hafize Öğüt <94183443+hafizeogut@users.noreply.github.com>
2023-11-21 03:14:46 +0300

    5_hafta

--- 00_Nesneye_Yonelik_Programlama_Ders_Uygulamalari/05_nesne_yonelimli_programlama.cpp/14_class_hastane.cpp
+++ 00_Nesneye_Yonelik_Programlama_Ders_Uygulamalari/05_nesne_yonelimli_programlama.cpp/14_class_hastane.cpp
@@ -21,7 +21,14 @@ int main(){
     h1.kimliklendir(25,"Sakarya");
     cout<<h1.getId()<<endl;
 
+    Hastane h2;
     h2.kimliklendir(98, "Sakarya");
     cout<<h2.getId()<<endl;
+
+    Hastane *h3=new Hastane;
+    h3->kimliklendir(12,"Bolu");
+    cout<<h3->getId()<<endl;
+
+    
 
 }
